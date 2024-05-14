6185ec9d9b78108238559be66edeea9341545a83
Nepta <63590772+Neptali-Ricardo@users.noreply.github.com>
2024-05-14 11:48:18 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ let cargarCabecero = () => {
 }
 
 const formatoMoneda = (valor) => {
-    return valor.toLocaleString('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 2});
+    return valor.toLocaleString('es-ES', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2});
 }
 
 const formatoPorcentaje = (valor) =>{
